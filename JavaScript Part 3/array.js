5f9c7f34524680acd038f2b042a6f1e752cba7d3
codingprahlad <prahladsomapurkar@gmail.com>
2025-08-18 23:36:08 +0530

    Add files via upload

---
+++ JavaScript Part 3/array.js
@@ -0,0 +1,15 @@
+// array in data structutre
+ let names = ["Prahlad","pavitra","Drashan"];
+ console.log (names);
+
+ let nums = [1,2,3,4];
+ console.log (nums);
+
+ let practice = ["sara",20,0.5];
+ console.log (practice);
+console.log( practice[0]);
+
+
+//Empty Array
+let empArr = [0];
+console.log (empArr);

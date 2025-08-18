21ff0c15f4beeb800dcbc9069308cdb8bebbf420
codingprahlad <prahladsomapurkar@gmail.com>
2025-08-18 21:58:09 +0530

    Add files via upload

---
+++ JavaScript Part 3/String Methods.js
@@ -0,0 +1,44 @@
+// trim method
+let str = " prahlad  ";
+console.log (str.trim());
+
+let b = " apna  collage  ";
+console.log (b.trim());
+
+let c = " apnacollage     ";
+console.log (c.trim());
+
+
+// toLowercase() and toUppercase()
+let d = "surya";
+console.log (d.toUpperCase());
+
+let e = "surya";
+console.log (e.toLowerCase());
+
+
+//method argument index of
+let f = "apnacollage";
+console.log (f.indexOf("apna"));
+
+let g = "Iam a richest person in the world";
+console.log (g.indexOf("person"));
+
+
+// slice method
+let s = "ilovecoding";
+console.log (s.slice(3,7));
+console.log (s.slice(5,11));
+console.log (s.slice(-3));
+
+
+// replace method
+let y = "Iam complan boy";
+console.log (y.replace("boy","girl"));
+console.log (y.replace("m","n"));
+ 
+
+// repeat method
+let z ="Gym";
+console.log (z.repeat(2));
+console.log (z.repeat(5));
---
+++ JavaScript Part 3/practice question.js
@@ -0,0 +1,12 @@
+let msg = "help";
+let newmsg = msg.trim();
+let newMsg = newmsg.toUpperCase();
+console.log(newMsg);
+
+let str = "shrinivas". trim().toUpperCase();
+console.log(str);
+
+let name = "apnacollege";
+console.log (name.slice(4,9));
+console.log (name.indexOf("na"));
+console.log (name.replace("apna","our"));

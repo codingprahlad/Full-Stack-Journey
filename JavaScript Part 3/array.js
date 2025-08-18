82bc95c01d119e68fd35f13682522a442bdf8e1b
codingprahlad <prahladsomapurkar@gmail.com>
2025-08-19 00:45:43 +0530

    Add files via upload

---
+++ JavaScript Part 3/array.js
@@ -0,0 +1,41 @@
+// array in data structutre
+ let names = ["Prahlad","pavitra","Drashan"];
+ console.log (names);
+ console.log (names[0].length);
+ console.log (names[0][0]);
+ console.log (names[0]);
+ console.log (names[0]= "Pavan");
+
+ let nums = [1,2,3,4];
+ console.log (nums);
+
+ let practice = ["sara",20,0.5];
+ console.log (practice);
+ console.log( practice[0]);
+
+
+//Empty Array
+let empArr = [0];
+console.log (empArr);
+
+// array method
+let vegetables = ["cucumber", "pumpkin", "beetroot", "gobi"];
+console.log(vegetables.push("onion")); 
+console.log(vegetables);
+console.log(vegetables.pop());
+console.log(vegetables);
+console.log(vegetables.unshift("ginger"));
+console.log(vegetables);
+console.log(vegetables.shift("gobi"));
+console.log(vegetables);
+
+// practice question
+let months = ["january","july","march","august"];
+console.log (months.shift());
+console.log (months);
+console.log (months.shift());
+console.log (months);
+console.log (months.unshift("june"));
+console.log (months);
+console.log (months.unshift("july"));
+console.log (months);

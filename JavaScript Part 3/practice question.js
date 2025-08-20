0f3c5624fd21d6c0b83c5b539659370dc99e8787
codingprahlad <prahladsomapurkar@gmail.com>
2025-08-20 20:06:08 +0530

    Update practice question.js

--- JavaScript Part 3/practice question.js
+++ JavaScript Part 3/practice question.js
@@ -10,3 +10,9 @@ let name = "apnacollege";
 console.log (name.slice(4,9));
 console.log (name.indexOf("na"));
 console.log (name.replace("apna","our"));
+
+// 2 numbers have the same last digit
+let ravi = 4632 ;
+let prashant = 1002 ;
+if
+

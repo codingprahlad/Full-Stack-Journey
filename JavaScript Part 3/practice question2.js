c9f9aa5456ef7deb46ca954570059455ecc47537
codingprahlad <prahladsomapurkar@gmail.com>
2025-09-08 20:29:18 +0530

    Add files via upload

---
+++ JavaScript Part 3/practice question2.js
@@ -0,0 +1,11 @@
+// change it to final form
+let month3 = ["janaury","july","march","august"];
+
+console.log(month3.splice(0,2, "july","june"));
+
+
+// reverse the elemengts
+let lang = ["c","c++","html","javascript","python","java","c#","sql"];
+
+console.log(lang.reverse());
+console.log(lang.indexOf("javascript"));

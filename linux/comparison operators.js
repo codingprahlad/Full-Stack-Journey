fa95ab1b3a543532ac4c054d79ff7d6e01f7c0c4
Prahlad Somapurkar <prahladsomapurkar@gmail.com>
2026-01-15 13:25:13 +0530

    Add files via upload

---
+++ linux/comparison operators.js
@@ -0,0 +1,33 @@
+"123" == 123
+
+1 == "1"
+
+0 == " "
+
+0 == false 
+
+null == undefined
+
+
+
+"123" === 123
+
+1 === "1"
+
+0 === " "
+
+0 === false 
+
+null === undefined
+
+// Unicode values
+"a" > "A"
+
+"a" > "b"
+
+"A" > "B"
+
+"*" > "&"
+
+
+
